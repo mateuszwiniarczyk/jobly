ab89fa2801d92aa5a2c1488a0a34e8ade89e8320
Mateusz Winiarczyk <mateuszwiniarczyk99@gmail.com>
2023-07-30 17:52:44 +0200

    feat: add eslint, prettier, husky, lint-staged and commit linting

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  arrowParens: 'always',
+  singleQuote: true,
+  jsxSingleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  semi: true,
+  plugins: [require('prettier-plugin-tailwindcss')],
+};
