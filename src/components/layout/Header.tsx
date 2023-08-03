b9272aff4b2352b8d4c6d27ce75823d2b6593d32
Mateusz Winiarczyk <mateuszwiniarczyk99@gmail.com>
2023-08-03 22:15:24 +0200

    feat: add svgr and logo icon

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,17 @@
+import Link from 'next/link';
+
+import LogoIcon from '~/logo.svg';
+
 export const Header = () => (
   <header className='sticky top-0 z-40 w-full border-b bg-background'>
-    <div className='container flex h-16 items-center'>
-      <h4>Header</h4>
+    <div className='container flex h-20 items-center py-4'>
+      <Logo />
     </div>
   </header>
+);
+
+const Logo = () => (
+  <Link href='/'>
+    <LogoIcon className='h-11 w-11' />
+  </Link>
 );
